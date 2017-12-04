a906a118fe58ed24823a0d988038142ea2beeae1
hod <hod246aaa@gmail.com>
2017-12-04 10:14:45 +0200

    first push

---
+++ src/test/java/bgu/spl/a2/PromiseTest.java
@@ -0,0 +1,34 @@
+package bgu.spl.a2;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PromiseTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void isResolved() {
+    }
+
+    @Test
+    public void resolve() {
+    }
+
+    @Test
+    public void subscribe() {
+    }
+}
---
+++ src/test/java/bgu/spl/a2/VersionMonitorTest.java
@@ -0,0 +1,30 @@
+package bgu.spl.a2;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class VersionMonitorTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getVersion() {
+    }
+
+    @Test
+    public void inc() {
+    }
+
+    @Test
+    public void await() {
+    }
+}
