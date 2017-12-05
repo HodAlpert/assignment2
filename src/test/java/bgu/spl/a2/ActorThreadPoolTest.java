895a9a5b5808673b4be7fed1d05835ee42f234c3
hod <hod246aaa@gmail.com>
2017-12-05 07:27:40 +0200

    added ActorThreadPool test template

---
+++ src/test/java/bgu/spl/a2/ActorThreadPoolTest.java
@@ -0,0 +1,20 @@
+package bgu.spl.a2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ActorThreadPoolTest {
+
+    @Test
+    public void submit() {
+    }
+
+    @Test
+    public void shutdown() {
+    }
+
+    @Test
+    public void start() {
+    }
+}
