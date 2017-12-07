7d8068b806a9ed8700ffed48ddac0604ac050905
omri <omrieitan@gmail.com>
2017-12-07 08:59:39 +0200

    added actionTest

---
+++ src/test/java/bgu/spl/a2/ActionTest.java
@@ -0,0 +1,30 @@
+package bgu.spl.a2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ActionTest {
+
+
+
+    @Test
+    public void handle() {
+    }
+
+    @Test
+    public void then() {
+    }
+
+    @Test
+    public void complete() {
+    }
+
+    @Test
+    public void getResult() {
+    }
+
+    @Test
+    public void sendMessage() {
+    }
+}
