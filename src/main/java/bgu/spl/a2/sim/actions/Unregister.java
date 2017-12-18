1886758d61487822f7d89b79528a71dae1dbd014
hod <hod246aaa@gmail.com>
2017-12-18 07:53:28 +0200

    added more actions tests

--- src/main/java/bgu/spl/a2/sim/actions/Unregister.java
+++ src/main/java/bgu/spl/a2/sim/actions/Unregister.java
@@ -24,7 +24,6 @@ public class Unregister extends Action<Boolean> {
             state.setAvailableSpots(state.getAvailableSpots()+1);//inc AvailableSpots
             state.setRegistered(state.getRegistered()-1);//reduce num of registered students.
             state.getRegStudents().remove(Student); // remove student from course's list
-
             List<Action<Boolean>> actions = new ArrayList<>();
             Action<Boolean> action = new SelfUnregisterStudent(Course);//asking student to remove self from course
             actions.add(action);
