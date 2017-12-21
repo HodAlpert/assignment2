82700e2b73ef017378f860a1c8084294ad1f8cac
hod <hod246aaa@gmail.com>
2017-12-21 21:02:01 +0200

    minor change in promise

--- src/main/java/bgu/spl/a2/sim/actions/ParticipateInCourse.java
+++ src/main/java/bgu/spl/a2/sim/actions/ParticipateInCourse.java
@@ -29,15 +29,23 @@ public class ParticipateInCourse extends Action<String[]> {
             Action<String[]> acceptToCourse = new AcceptToCourse(this.Course, this.Grade[0], state.getPrequisites());
             List<Action<String[]>> actions = new ArrayList<>();
             actions.add(acceptToCourse);
-            then(actions, () -> {
+            state.getRegStudents().add(Student);//TODO- manage deleting student
+            then(actions, () ->{
                     if (!acceptToCourse.getResult().get()[0].equals("-") && state.getAvailableSpots() >0) {
+                        //if student should be registered
                         state.setRegistered(state.getRegistered() + 1);
                         state.setAvailableSpots(state.getAvailableSpots()-1);
-                        state.getRegStudents().add(Student);
                         this.complete(acceptToCourse.getResult().get());
                     }
-                    else if (state.getAvailableSpots() <1)
-                        this.complete(result);
+                    else if (state.getAvailableSpots() <1&&!acceptToCourse.getResult().get()[0].equals("-")) {
+                        SelfUnregisterStudent unregister = new SelfUnregisterStudent(Course);
+                        List<Action<Boolean>> actions1 = new ArrayList<>();
+                        actions1.add(unregister);
+                        then(actions1,()->{
+                            this.complete(result);
+                        });
+                        sendMessage(unregister,Student,new StudentPrivateState());
+                    }
                     else
                         this.complete(acceptToCourse.getResult().get());
 
